ad602ec3a1a909089378c9c6ac41706811249f25
jorjean2901 <thanaporn.kaewkh@ku.th>
2025-07-14 16:11:33 +0700

    Add files via upload

--- Lib/CartItem.java
+++ Lib/CartItem.java
@@ -1,4 +1,17 @@
+import java.util.ArrayList;
+
 public record CartItem(String sku, String name, double price, int quantity){
+    public class CartI{
+        private ArrayList<CartItem>items = new ArrayList<>();
+        
+        public void addItem (CartItem item){
+            items.add(item);
+        }
+        public ArrayList<CartItem> getItem(){
+            return items;
+        }
+    }
+
 
     
 
